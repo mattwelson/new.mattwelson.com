3e92b8561d532c8b2224e96165c5f66f0a20ce32
Matt Welson <matthew.welson@gmail.com>
2019-11-01 16:06:39 +1300

    feat(theme-dev): Switching tag page to a real page

--- packages/gatsby-theme-blog-matt/src/components/pages/tags-page.js
+++ packages/gatsby-theme-blog-matt/src/components/pages/tags-page.js
@@ -1,3 +1,28 @@
-import React from 'react'
+/** @jsx jsx */
+import { jsx, Styled } from 'theme-ui'
+import { Fragment } from 'react'
+import { Link } from 'gatsby'
+import { Layout } from 'gatsby-theme-ui-layout'
 
-export default props => <pre>{JSON.stringify(props.data, null, 2)}</pre>
+export default ({ tags, ...props }) => {
+  return (
+    <Styled.root>
+      <Layout {...props}>
+        {tags.map(tag => (
+          <Fragment key={tag.name}>
+            <h2>{tag.name}</h2>
+            <ul>
+              {tag.posts.map(post => (
+                <li key={post.id}>
+                  <Link to={post.slug}>{post.title}</Link>
+                  <div>{post.date}</div>
+                  <div>{post.excerpt}</div>
+                </li>
+              ))}
+            </ul>
+          </Fragment>
+        ))}
+      </Layout>
+    </Styled.root>
+  )
+}
--- packages/gatsby-theme-blog-matt/src/templates/tags-page-template.js
+++ packages/gatsby-theme-blog-matt/src/templates/tags-page-template.js
@@ -1,13 +1,12 @@
+import React from 'react'
 import { graphql } from 'gatsby'
 import TagsPage from '../components/pages/tags-page'
-
-export default TagsPage
 
 export const query = graphql`
   query TagPage {
     allBlogPost {
       group(field: tags) {
-        nodes {
+        posts: nodes {
           id
           title
           slug
@@ -17,3 +16,9 @@ export const query = graphql`
     }
   }
 `
+
+const TagsPageTemplate = ({ data }) => (
+  <TagsPage tags={data.allBlogPost.group} />
+)
+
+export default TagsPageTemplate
